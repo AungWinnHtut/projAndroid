6faf9864e047ba4c055a88ad7bc4d98fc77d5cb5
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2015-08-24 20:01:25 +0630

    android json add search feature

--- com.engineer4myanmar.json.MainActivity/gen/com/engineer4myanmar/json/R.java
+++ com.engineer4myanmar.json.MainActivity/gen/com/engineer4myanmar/json/R.java
@@ -25,16 +25,21 @@ public final class R {
     public static final class id {
         public static final int btnLogin=0x7f080002;
         public static final int btnRegister=0x7f080003;
-        public static final int btnSearch=0x7f080011;
+        public static final int btnSearch=0x7f080016;
         public static final int etCity=0x7f08000f;
         public static final int etDob=0x7f08000b;
         public static final int etEmail=0x7f080007;
+        public static final int etMax=0x7f080014;
+        public static final int etMin=0x7f080013;
         public static final int etPassword=0x7f080001;
         public static final int etPhoneno=0x7f080009;
         public static final int etProfession=0x7f08000d;
         public static final int etUsername=0x7f080000;
-        public static final int menu_settings=0x7f080012;
-        public static final int spinner1=0x7f080010;
+        public static final int menu_settings=0x7f080017;
+        public static final int spCuisine=0x7f080015;
+        public static final int spRange=0x7f080011;
+        public static final int spRating=0x7f080012;
+        public static final int spServices=0x7f080010;
         public static final int textView1=0x7f080004;
         public static final int textView2=0x7f080005;
         public static final int textView3=0x7f080006;
